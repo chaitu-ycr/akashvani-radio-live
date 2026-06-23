a52e918d3fdc2e6fcf06f3b7a7c56a2ccee41a90
chaitu-ycr <chaitu-ycr@gmail.com>
2026-06-24 00:23:29 +0530

    build command updated

--- scripts/build_app_exe.bat
+++ scripts/build_app_exe.bat
@@ -2,4 +2,4 @@ cd %~dp0\..
 
 uv sync --upgrade
 
-uv run pyinstaller --onefile --noconsole --icon=.\src\akashvani_radio_live\app_icon.ico --collect-all pywebview --name akashvani-radio-live .\src\akashvani_radio_live\app.py
+uv run pyinstaller --onefile --noconsole --icon=.\src\akashvani_radio_live\app_icon.ico --name akashvani-radio-live .\src\akashvani_radio_live\app.py
